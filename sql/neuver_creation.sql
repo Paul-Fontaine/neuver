d4afa525c02dd7a702d7b37839794d7e5b732a23
Paul Fontaine <fontainepaul36@gmail.com>
2023-05-31 14:52:44 +0200

    default values for dates

--- sql/neuver_creation.sql
+++ sql/neuver_creation.sql
@@ -14,7 +14,7 @@ CREATE TABLE public.user(
 	age            INT  NOT NULL ,
 	mail           VARCHAR (50) NOT NULL ,
 	mdp            VARCHAR (150) NOT NULL ,
-	photo_profil   VARCHAR (150)   ,
+	photo_profil   VARCHAR (150)  DEFAULT '/ressources/images/users_pp/default_user_pp.png',
 	CONSTRAINT user_PK PRIMARY KEY (id_user)
 )WITHOUT OIDS;
 
@@ -27,7 +27,7 @@ CREATE TABLE public.artiste(
 	nom_artiste           VARCHAR (50) NOT NULL ,
 	type_artiste          VARCHAR (50) NOT NULL ,
 	description_artiste   VARCHAR (50) NOT NULL ,
-	photo_artiste         VARCHAR (150)   ,
+	photo_artiste         VARCHAR (150)  DEFAULT '/ressources/images/artistes_pp/default_artiste_pp.png',
 	CONSTRAINT artiste_PK PRIMARY KEY (id_artiste)
 )WITHOUT OIDS;
 
@@ -40,7 +40,7 @@ CREATE TABLE public.album(
 	nom_album             VARCHAR (50) NOT NULL ,
 	date_parution_album   VARCHAR (50) NOT NULL ,
 	style_album           VARCHAR (50) NOT NULL ,
-	cover_album           VARCHAR (150)  ,
+	cover_album           VARCHAR (150) DEFAULT '/ressources/images/album_covers/default_album_cover.png',
 	id_artiste            INT  NOT NULL  ,
 	CONSTRAINT album_PK PRIMARY KEY (id_album)
 
@@ -56,7 +56,7 @@ CREATE TABLE public.morceau(
 	nom_morceau     VARCHAR (50) NOT NULL ,
 	duree_morceau   VARCHAR (50) NOT NULL ,
 	lien            VARCHAR (150) NOT NULL ,
-	explicit        BOOL  NOT NULL ,
+	explicit        BOOL  NOT NULL DEFAULT FALSE,
 	id_album        INT  NOT NULL  ,
 	CONSTRAINT morceau_PK PRIMARY KEY (id_morceau)
 
@@ -70,7 +70,7 @@ CREATE TABLE public.morceau(
 CREATE TABLE public.playlist(
 	id_playlist      SERIAL NOT NULL ,
 	nom_playlist     VARCHAR (50) NOT NULL ,
-	photo_playlist   VARCHAR (150)   ,
+	photo_playlist   VARCHAR (150)  DEFAULT '/ressources/images/playlists_photo/default_playlist_photo.png',
 	CONSTRAINT playlist_PK PRIMARY KEY (id_playlist)
 )WITHOUT OIDS;
 
@@ -81,7 +81,7 @@ CREATE TABLE public.playlist(
 CREATE TABLE public.user_playlist(
 	id_playlist              INT  NOT NULL ,
 	id_user                  INT  NOT NULL ,
-	date_creation_playlist   DATE  NOT NULL  ,
+	date_creation_playlist   DATE  NOT NULL DEFAULT current_timestamp,
 	CONSTRAINT user_playlist_PK PRIMARY KEY (id_playlist,id_user)
 
 	,CONSTRAINT user_playlist_playlist_FK FOREIGN KEY (id_playlist) REFERENCES public.playlist(id_playlist)
@@ -95,7 +95,7 @@ CREATE TABLE public.user_playlist(
 CREATE TABLE public.playlist_morceau(
 	id_morceau            INT  NOT NULL ,
 	id_playlist           INT  NOT NULL ,
-	date_ajout_playlist   DATE  NOT NULL  ,
+	date_ajout_playlist   DATE  NOT NULL DEFAULT current_timestamp ,
 	CONSTRAINT playlist_morceau_PK PRIMARY KEY (id_morceau,id_playlist)
 
 	,CONSTRAINT playlist_morceau_morceau_FK FOREIGN KEY (id_morceau) REFERENCES public.morceau(id_morceau)
