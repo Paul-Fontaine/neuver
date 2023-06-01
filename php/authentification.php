61143ef7b41cf0859618f31f5f143172c1da3345
Paul Fontaine <fontainepaul36@gmail.com>
2023-06-01 10:20:33 +0200

    function addUser (in the db)

--- php/authentification.php
+++ php/authentification.php
@@ -30,3 +30,39 @@ function auth_state(string $mail, string $mdp){
     }
     return "connected";
 }
+
+/**
+ * create a new user in the db
+ * @param string $mail
+ * @param string $prenom
+ * @param string $nom
+ * @param string $date_naissance
+ * @param        $mdp
+ * @return bool true on success | false on failure
+ */
+function addUser(string $mail, string $prenom, string $nom, string $date_naissance, string $mdp): bool
+{
+    try {
+        $db = DB::connexion();
+
+        $request = '
+        INSERT INTO "user"(prenom, nom, date_naissance, mail, mdp) VALUES 
+        (:prenom, :nom, :date_naissance, :mail, :mdp);
+        ';
+        $statement = $db->prepare($request);
+        $statement->bindParam(':prenom', $prenom);
+        $statement->bindParam(':nom', $nom);
+        $statement->bindParam('$:date_naissance', $date_naissance);
+        $statement->bindParam(':mail', $mail);
+        $statement->bindParam(':mdp', $mdp);
+        $statement->execute();
+
+        return true;
+    }
+    catch (PDOException $exception)
+    {
+        error_log('Request error: '.$exception->getMessage());
+        return false;
+    }
+
+}
