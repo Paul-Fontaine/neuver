6052f89e654eca48f1dff36d12bfba7f99c7e60b
Paul Fontaine <fontainepaul36@gmail.com>
2023-06-01 08:44:19 +0200

    add authentification.php

---
+++ php/authentification.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+require_once 'DB.php';
+
+function auth_state(string $mail, string $mdp){
+    $db = DB::connexion();
+    if (empty($_SESSION['id_user'])){
+
+        if (!empty($mail) and !empty($mdp)){
+
+            $request = '
+            SELECT id_user FROM user
+            WHERE mail =:mail AND mdp=crypt(:mdp,mdp)
+            ';
+            $statement = $db->prepare($request);
+            $statement->bindParam(':username', $mail);
+            $statement->bindParam(':mdp', $mdp);
+            $statement->execute();
+
+            $result = $statement->fetch();
+
+            if (!empty($result['id_user'])){
+                $_SESSION['id_user'] = $result['id_user'];
+                return 'connected';
+            }
+            return "incorrect";
+        }
+        return "not connected";
+    }
+    return "connected";
+}
