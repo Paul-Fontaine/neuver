41a738f297956130b8aa24cc3a62dd2cbfee30ed
Paul Fontaine <fontainepaul36@gmail.com>
2023-06-02 09:12:00 +0200

    favoris est une clé étrangère de la table utilisateur. modification pour l'insertion + bonus : try catch qui manquait

--- php/user.php
+++ php/user.php
@@ -33,6 +33,7 @@ class User
         return $now->diff($birthday)->y;
     }
 
+
     /**
      * tell if a user is connected or if its pwd is correct
      * @param string $mail
@@ -67,6 +68,7 @@ class User
         return "connected";
     }
 
+
     /**
      * create a new user in the db. It will also create a new playlist favoris and link it with the new user
      * @param string $mail
@@ -81,38 +83,28 @@ class User
         try {
             $db = DB::connexion();
 
-            // create the new user and get its id_utilisateur
-            $request = "
-        INSERT INTO utilisateur(prenom, nom, date_naissance, mail, mdp) VALUES 
-        (:prenom, :nom, :date_naissance, :mail, crypt('$mdp', gen_salt('md5')) )
-        RETURNING id_utilisateur;
-        ";
-            $statement = $db->prepare($request);
-            $statement->bindParam(':prenom', $prenom);
-            $statement->bindParam(':nom', $nom);
-            $statement->bindParam(':date_naissance', $date_naissance);
-            $statement->bindParam(':mail', $mail);
-            $statement->execute();
-
-            $id_new_user = $statement->fetch(PDO::FETCH_NUM)[0];
-
             // create a new playlist named 'favoris' and get its id_playlist
             $request = "
-        INSERT INTO playlist(nom_playlist, photo_playlist) VALUES 
-        ('favoris', '/ressources/images/playlists_photo/favoris.png')
-        RETURNING id_playlist;
-        ";
+            INSERT INTO playlist(nom_playlist, photo_playlist) VALUES 
+            ('favoris', '/ressources/images/playlists_photo/favoris.png')
+            RETURNING id_playlist;
+            ";
             $statement = $db->prepare($request);
             $statement->execute();
 
             $id_favoris_new_user = $statement->fetch(PDO::FETCH_NUM)[0];
 
-            // link the new user and its favoris playlist
+            // create the new user and get its id_utilisateur
             $request = "
-        INSERT INTO user_playlist(id_playlist, id_utilisateur) VALUES 
-        ($id_favoris_new_user, $id_new_user)
-        ";
+            INSERT INTO utilisateur(prenom, nom, date_naissance, mail, mdp, id_playlist_favoris) VALUES 
+            (:prenom, :nom, :date_naissance, :mail, crypt('$mdp', gen_salt('md5')), :favoris)
+            ;";
             $statement = $db->prepare($request);
+            $statement->bindParam(':prenom', $prenom);
+            $statement->bindParam(':nom', $nom);
+            $statement->bindParam(':date_naissance', $date_naissance);
+            $statement->bindParam(':mail', $mail);
+            $statement->bindParam(':favoris', $id_favoris_new_user);
             $statement->execute();
 
             return true;
@@ -124,25 +116,38 @@ class User
         }
     }
 
+
     /**
-     * @return array the ten last songs listenned (their id)
+     * We'll probably won't use the m.id_morceau and should consider remove it later
+     * @return array|false the ten last songs listenned
      */
     function recemment_ecoutes()
     {
-        $db = DB::connexion();
-        $request = "
-        SELECT id_morceau FROM recemment_ecoutes
-        WHERE id_utilisateur = :id_utilisateur
-        ;";
-        $statement = $db->prepare($request);
-        $statement->bindParam(':id_utilisateur', $this->id_utilisateur);
-        $statement->execute();
-        $result =  $statement->fetchAll(PDO::FETCH_NUM);
-        $songs = array();
-        foreach ($result as $id_song){
-            array_push($songs, $id_song[0]);
+        try {
+            $db = DB::connexion();
+            $request = "
+            SELECT m.id_morceau,
+                   m.nom_morceau,
+                   m.duree_morceau,
+                   m.lien,
+                   m.explicit,
+                   m.id_album
+            FROM recemment_ecoutes re
+            JOIN morceau m on m.id_morceau = re.id_morceau
+            WHERE re.id_utilisateur = :id_utilisateur
+            ;";
+            $statement = $db->prepare($request);
+            $statement->bindParam(':id_utilisateur', $this->id_utilisateur);
+            $statement->execute();
+            $result = $statement->fetchAll(PDO::FETCH_NUM);
+
+            return $result;
         }
-        return $songs;
+        catch (PDOException $exception)
+        {
+            error_log('Request error: '.$exception->getMessage());
+            return false;
+        }
     }
 }
 
