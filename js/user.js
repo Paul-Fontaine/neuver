60298a33f8bba880e736f9678fc1743de539e541
rcollo25 <71321505+rcollo25@users.noreply.github.com>
2023-06-05 00:23:27 +0200

    commit all my change ; recherche done and version_fusion done

--- js/user.js
+++ js/user.js
@@ -3,17 +3,17 @@
 $('#button-se-connecter').on("click", () => {
     let mail = $('#Email').val();
     let mdp = $('#Password').val();
-
+  
     ajaxRequest(
         'GET',
         '../php/request.php/authentification',
         authentification,
         'mail='+mail+'&mdp='+mdp
     );
-})
-
-function authentification(data)
-{
+  })
+  
+  function authentification(data)
+  {
     switch (data){
         case 'connected':
             window.location.href = "accueil.html";
@@ -22,33 +22,33 @@ function authentification(data)
             $('#alert-erreur-connexion').toggleClass('d-none');
             break;
     }
-}
-
-
-//=========================RYAN===================================
-
-$('#button_page_inscription').on("click", () => {
+  }
+  
+  
+  //=========================RYAN===================================
+  
+  $('#button_page_inscription').on("click", () => {
     window.location.href = 'inscription.html';
-})
-
-$('#button-s-inscire').on("click", () => {
+  })
+  
+  $('#button-s-inscire').on("click", () => {
     let prenom = $('#prenom_inscr').val();
     let nom = $('#nom_inscr').val();
     let date_naissance = $('#birthdate_inscr').val();
     let mail = $('#mail_inscr').val();
     let mdp = $('#pwd_inscr').val();
     let mdp_conf = $('#pwd_inscr_conf').val();
-
+  
     ajaxRequest(
         'POST',
         '../php/request.php/inscription',
         inscription,
         'prenom='+prenom+'&nom='+nom+'&date_naissance='+date_naissance+'&mail='+mail+'&mdp='+mdp+'&mdp_conf='+mdp_conf
     );
-})
-
-function inscription(data)
-{
+  })
+  
+  function inscription(data)
+  {
     switch (data){
         case 'inscrit':
             window.location.href = "authentification.html";
@@ -60,16 +60,17 @@ function inscription(data)
             $('#alert-erreur-mdp').toggleClass('d-none');
             break;
     }
-}
-
-
-
-
-
-
-
-
-//========================Gauthier===================================
-
-
-
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  //========================Gauthier===================================
+  
+  
+  
+  
