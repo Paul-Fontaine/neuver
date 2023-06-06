7e74a1d0a279d5f4cac7e458b5628566d195c067
Ryan Collobert <ryan.collobert@isen-ouest.yncrea.fr>
2023-06-06 13:33:47 +0200

    constants

--- php/constants.php
+++ php/constants.php
@@ -1,7 +1,7 @@
 <?php
 // Database constants.
-const DB_USER = 'postgres';
-const DB_PASSWORD = 'Isen44N';
+const DB_USER = 'neuver';
+const DB_PASSWORD = 'neuver';
 const DB_NAME = 'neuver';
 const DB_SERVER = '127.0.0.1';
 const DB_PORT = '5432';
